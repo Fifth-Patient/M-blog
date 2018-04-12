00760e27f9de0cb656108dd13fe84510897e5e4b
5.th <1923407743@qq.com>
2018-04-13 04:21:48 +0800

    connect mongodb and wire signup.jade

--- routes/signup.js
+++ routes/signup.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 router.get('/signup', checkNotLogin, (req, res, next) => {
-  res.send('注册页面')
+  res.render('signup')
 })
 
 router.post('/signup', checkNotLogin, (req, res, next) => {
