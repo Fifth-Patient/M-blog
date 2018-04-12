bbd01df034d92d29e761965fd1ec0250389de54b
5.th <1923407743@qq.com>
2018-04-13 04:54:20 +0800

    use signup jade

--- routes/signin.js
+++ routes/signin.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 router.get('/signin', checkNotLogin, (req, res, next) => {
-  res.send('登陆页面')
+  res.reader('signin')
 })
 
 router.post('/signin', checkNotLogin, (req, res, next) => {
